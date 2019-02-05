c508396cea3409601c3aea398b1fa94b8ad1a2d4
chrishewison1983 <ods.chrishewison@gmail.com>
2019-02-05 08:36:39 +0000

    Persona journey change and FAA CSS changes

--- dist/js/my-cookies.js
+++ dist/js/my-cookies.js
@@ -1,5 +1,34 @@
 // =================================== MY COOKIES =================================== //
 
+/////////////////// INDEX PAGE COOKIES - START ///////////////////
+// Stores the cookie
+$("header nav .navigation__link--top-level--apprentice, #apprentice-journey").on("click", function (e) {
+     $.cookie("apprentice-journey", true, {path:'/'});
+     $.cookie("employer-journey", false, {path:'/'});
+});
+
+
+$("header nav .navigation__link--top-level--employer, #employer-journey").on("click", function (e) {
+     $.cookie("employer-journey", true, {path:'/'});
+     $.cookie("apprentice-journey", false, {path:'/'});
+});
+
+// Populates the fields
+if ($.cookie("apprentice-journey") == 'true') {
+     $("#apprentice-journey, #employer-journey, #employer-journey-2").hide();
+     $("#apprentice-journey, #apprentice-journey-2, #apprentice-journey-3").show();
+} else if ($.cookie("employer-journey") == 'true') {
+     $("#employer-journey, #employer-journey-2").show();
+     $("#apprentice-journey, #apprentice-journey-2, #apprentice-journey-3").hide();
+} else {
+     $("#apprentice-journey, #employer-journey").show();
+     $("#apprentice-journey-2, #apprentice-journey-3, #employer-journey-2").hide();
+}
+/////////////////// INDEX PAGE COOKIES - END ///////////////////
+
+
+
+/////////////////// FAA COOKIES - START ///////////////////
 // Stores the cookie
 $("#link-FAA-search").on("click", function (e) {
      $.cookie("faa-search-location", true, {path:'/'});
@@ -9,3 +38,4 @@ $("#link-FAA-search").on("click", function (e) {
 
 // Populates the fields
 if ($.cookie("faa-search-location")) { $("#Postcode").val($.cookie("faa-search-location")); }
+/////////////////// FAA COOKIES - END ///////////////////
