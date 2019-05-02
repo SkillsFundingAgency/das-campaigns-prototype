97882845b2a13a9bcb49bad1d5afa2718f3aa31a
chrishewison1983 <ods.chrishewison@gmail.com>
2019-05-02 14:49:25 +0100

    Updates to the FAT filter

--- dist/js/my-cookies.js
+++ dist/js/my-cookies.js
@@ -71,7 +71,7 @@ $("#link-FAT-search").on("click", function (e) {
 // Populates the fields
 if ($.cookie("fat-job-title")) {
      $(".hero.fat-hero").find('.fat-job-title').text($.cookie("fat-job-title"));
-     $(".hero.fat-hero").find('#fat-search-keyword').val($.cookie("fat-job-title"));
+     $('#fat-search-keyword').val($.cookie("fat-job-title"));
 }
 
 // SECTORS ROUTE
