4847a3ee4babc6efab1dd982755486dbe1477e42
chrishewison1983 <chris@chewitdesign.co.uk>
2019-08-19 14:31:48 +0100

    Updates to new case studies component

--- dist/js/my-jquery.js
+++ dist/js/my-jquery.js
@@ -522,6 +522,34 @@ $(document).ready(function () {
      // $('.small-quotes-container').slick({
      // });
 
+
 });
+
+$(function () {
+    $("#quoteOne").css("display", "block");
+    $("div.tab button:first-child" ).addClass(" active");
+    $(".content").click(function () {
+        $(this).animate({opacity: "0"}, 200);
+        var links = document.getElementsByClassName("links");
+        for (i = 0; i < links.length; i++) {
+        links[i].className = links[i].className.replace(" active", "");
+        }
+    });
+});
+
+function openQuote(e, quote) {
+    var links = document.getElementsByClassName("links");
+    var content = document.getElementsByClassName("content");
+    for (i = 0; i < content.length; i++) {
+        content[i].style.display = "none";
+    }
+    for (i = 0; i < links.length; i++) {
+        links[i].className = links[i].className.replace(" active", "");
+    }
+    document.getElementById(quote).style.display = "block";
+    document.getElementById(quote).style.opacity = "1";
+    e.currentTarget.className += " active";
+}
+
 
 ////////////////////////////// HUB PAGE - END //////////////////////////////
