e34afbb763ab5d110545caeac11b3a4fbaea2227
chrishewison1983 <chris@chewitdesign.co.uk>
2019-12-10 11:33:32 +0000

    Updates to new imagery

--- dist/js/my-jquery.js
+++ dist/js/my-jquery.js
@@ -558,6 +558,16 @@ function openQuote(e, quote) {
      e.currentTarget.className += " active";
 }
 
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+     $('#slideshow > div:first')
+          .fadeOut(1000)
+          .next()
+          .fadeIn(1000)
+          .end()
+          .appendTo('#slideshow');
+}, 3000);
 
 ////////////////////////////// HUB PAGE - END //////////////////////////////
 
